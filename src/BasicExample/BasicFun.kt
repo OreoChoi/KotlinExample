aeb2ad563daf40b12bac1dd6e3bad68df1f8c13c
junho <chlwnsgh311@gmail.com>
2020-06-10 16:06:18 +0900

    [코틀린 기본]
     1. BasicFun.kt
        코틀린의 기본 문법을 정리한 파일 입니다.

---
+++ src/BasicExample/BasicFun.kt
@@ -0,0 +1,83 @@
+package BasicExample
+
+
+/**
+ * 1. 기본적인 함수 정의
+ * */
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+/**
+ * 2. 식 몸체 (expression body) 를 사용한 함수 정의
+ * */
+fun minus(a: Int, b: Int) = a - b
+
+/**
+ * 3. 의미 없는 값을 리턴하는 함수
+ *
+ *  - Unit 타입은 자바의 Void와 유사하며 의미 없는 값을 리턴한다
+ *  - Unit은 선언을 생략이 가능하다.
+ * */
+fun printHello(): Unit {
+    println("Hello Kotlin World!!!")
+}
+
+/**
+ * 4. val 예제
+ *
+ *  - 1회만 초기화 가능한(mutable) 수 이다
+ *  - var과는 반대의 속성을 지니고 있다
+ * */
+fun valExample() {
+    val a: Int = 1  // Int형 변수에 즉시 값 할당
+    val b = 2       // Int형으로 추론 및 값 할당
+    val c: Int       // Int형 변수 선언
+    c = 3           // 값 할당
+}
+
+
+/**
+ * 5. var 예제
+ *
+ *  - 가변(immutable) 수 이다
+ *  - val과는 반대의 속성을 지니고 있다
+ * */
+fun varExample() {
+    var a: Int = 1
+    a = 3 + 2
+}
+
+/**
+ * 6. 주석
+ *   - 실제 프로그램에 사용되는 문구는 아니지만 프로그램의 해석을 돕는
+ * */
+fun comment(){
+    // 줄 주석 이라고 한다.
+
+    /*
+    * 주석 블록 이라고 한다.
+    * */
+
+    /**
+     * 문서화 주석 이라고 하며
+     * reference Doc을 만들기 위해 사용된다.
+     * */
+}
+
+/**
+ * 7. 문자열
+ *    - 문자열 사용 방식
+ * */
+fun toString(){
+    val num = 10
+    println("num = $num")   //num 변수를 이와 같이 출력 가능하다
+
+    val age:String = "Ten"
+    println("$age.lenght is ${age.length}") //임의의 식도 사용가능
+
+    val introduction:String = """안녕하세요 현재 사용하는 문자열은
+        개행 문자가 없더라도 자동으로 개행을 넣어서 표시가 가능합니다
+        ...."""
+    println(introduction)
+}
---
+++ src/BasicExample/Main.kt
@@ -0,0 +1,16 @@
+package BasicExample
+
+/**
+ * Main - 20200610
+ * 프로그램 시작점
+ * */
+fun main(args: Array<String>) {
+
+    //BasicExample
+    println(sum(1, 1))
+    println(minus(1, 1))
+    printHello()
+    valExample()
+    toString()
+}
+
